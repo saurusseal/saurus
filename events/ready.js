7e77d2409577151b250342c90c7297d92158d165
saurusseal <49242725+saurusseal@users.noreply.github.com>
2019-04-03 10:01:29 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 const chalk = require("chalk");
 module.exports = async (client) => {
-    console.log(chalk.green('Iridine loaded successfully.'));
-    await client.user.setPresence({ game: { name: `i.help`}});
+    console.log(chalk.green('Saurus loaded successfully.'));
+    await client.user.setPresence({ game: { name: `${client.users.count} Dinosaur Earthers`}});
 }
 
