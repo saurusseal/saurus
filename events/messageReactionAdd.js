0b65088dae91c4330972ff3d2688e1c8b5e5b39f
saurusseal <49242725+saurusseal@users.noreply.github.com>
2019-04-04 12:21:05 -0500

    Update messageReactionAdd.js

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -6,7 +6,7 @@ module.exports = async (client, reaction, user) => {
 const embed = await new Discord.RichEmbed().setDescription("").setColor(client.resolver.resolveColor('YELLOW'));
 let starboard = client.channels.find(c => c.name == "starboard");
 
-starboard.send(embed.setDescription(reaction.message.content).setName(reaction.message.author.username).setThumbnail(reaction.message.member.user.avatarURL));
+starboard.send(embed.setDescription(reaction.message.content).setAuthor(reaction.message.author.username, reaction.message.member.user.avatarURL));
     }
   }
 };
