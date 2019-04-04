bef99cbf8556da8128aa40f6fe0e9cca6ceef46f
saurusseal <49242725+saurusseal@users.noreply.github.com>
2019-04-04 12:12:10 -0500

    Update messageReactionAdd.js

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,11 +1,12 @@
 const Discord = require('discord.js')
 
-module.exports = async (reaction, user) => {
+module.exports = async (client, reaction, user) => {
   if (reaction.emoji.name === "⭐") {
-    if(reaction.count == 3) {
+    if(reaction.count == 1) {
 const embed = await new Discord.RichEmbed().setDescription("").setColor(client.resolver.resolveColor('YELLOW'));
+let starboard = client.channels.find(c => c.name == "starboard");
 
-
+starboard.send(embed.setDescription(reaction.message.content));
     }
   }
 };
