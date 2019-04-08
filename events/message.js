c3f6a6de862355bc0ef88921dc7aac88d70650bc
saurusseal <49242725+saurusseal@users.noreply.github.com>
2019-04-07 21:10:02 -0500

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,7 +1,17 @@
+var cleverbot = require("cleverbot.io"),
+bot = new cleverbot(process.env.cleverbotuser, process.env.cleverbotkey);
+
 module.exports = async (client, message) => {
 let messageArray = message.content.split(" ");
 let cmd = messageArray[0].toLowerCase();
 if(message.author.bot) return;
+  if(message.channel.id == 564631599476506639) {
+  bot.create(function (err, session) {
+  bot.ask(messageArray.join(" "), function (err, response) {
+  message.channel.send(response);
+});
+});
+  }
 if(message.content.toLowerCase().indexOf(client.config.prefix) !== 0) return;
 const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
 const command = args.shift().toLowerCase();
